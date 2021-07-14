b192634f32035a4349536c1e3a7b493f5d012bc0
신종신 <jongsin16@naver.com>
2021-07-14 11:20:53 +0900

    day21

---
+++ for3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+    while(true){
+        cin >> a >> b;
+        if((a == 0) && (b == 0))
+            break;
+        cout << a + b << '\n';
+    }
+    return 0;
+}
