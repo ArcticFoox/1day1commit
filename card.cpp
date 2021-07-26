6f1f8666e47cacdb5a47a6ede7eccb67052537bd
신종신 <jongsin16@naver.com>
2021-07-26 09:41:29 +0900

    day32

---
+++ card.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int sum = 0;
+    int n;
+    
+    for(int i = 0; i < 5; i++){
+        scanf("%d", &n);
+        sum += n;
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
