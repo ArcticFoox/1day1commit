6255a4216bf75bda051db2ff8bf54b1890ed30d1
신종신 <jongsin16@naver.com>
2021-07-31 15:41:25 +0900

    day37

---
+++ domino.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    int sum = 0;
+    scanf("%d", &n);
+
+    for(int i = 0; i <= n; i++){
+        for(int j = 0; j <= i; j++){
+            sum += i + j;
+        }
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
