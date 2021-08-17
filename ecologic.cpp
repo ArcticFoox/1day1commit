0daef57986a8803146b0f6ef759be279773be7c5
신종신 <jongsin16@naver.com>
2021-08-17 14:46:55 +0900

    day53

---
+++ ecologic.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<map>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    
+    map<string, double>arr;
+    int count = 0;
+    string name;
+
+    while(getline(cin, name)){
+        arr[name]++;
+        count++;
+    }
+    
+    cout<<fixed;
+    cout.precision(4);     // 소수점 자리 4자리 고정시키기
+
+    for(auto it = arr.begin(); it != arr.end(); it++){
+        string spicise = it->first;
+        double pst = it->second;
+
+        pst/=count;
+        pst*=100;
+
+        cout << spicise << pst << '\n';
+    }
+
+    return 0;
+}
+// https://mapocodingpark.blogspot.com/2020/01/BOJ-4358.html
