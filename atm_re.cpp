39caff2eb2a02713cf6fd16f6a3c6889bf6e45c1
신종신 <jongsin16@naver.com>
2022-03-16 20:58:05 +0900

    11399

---
+++ atm_re.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    int sum = 0;
+    int arr[1000];
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    sort(arr, arr+n);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i; j++){
+            sum += arr[j];
+        }
+    }
+    cout << sum << '\n';
+    return 0;
+}
